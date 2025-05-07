95ce8058563b2e8d019c4fb109a4e9879c70f8b8
Ivan Morales <ivanymz09@gmail.com>
2025-05-06 19:06:27 -0600

    Student statement route added

--- src/lib/apiRoutes.ts
+++ src/lib/apiRoutes.ts
@@ -4,6 +4,7 @@ export const API_ROUTES = {
     AUTH_SIGNOUT: `${API_BASE}/api/auth/signout`,
     STUDENT_REQUEST_STATUS: `${API_BASE}/api/students_info/request_status`,
     STUDENTS_LIST: `${API_BASE}/api/students_info/requests`,
+    STUDENTS_UPLOAD_STATEMENT: `${API_BASE}/api/students_info/transcription/upload_statement`,
     STUDENT_FILES: (id: string) => `${API_BASE}/api/students_info/details/student_files/${id}`,
     STUDENT_DETAILS: (id: string) => `${API_BASE}/api/students_info/details/${id}`,
     STUDENT_STATEMENT: (id: string) => `${API_BASE}/api/students_info/transcription/${id}`,  
